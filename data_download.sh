4bf8f99428c570f48e0ae913c931e1b2f94a70a1
LYH-YF <yifan2250@gmail.com>
2020-11-11 14:30:11 +0800

    first commit

---
+++ data_download.sh
@@ -0,0 +1,5 @@
+wget -O ./data/train_set.csv.zip https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531810/train_set.csv.zip
+wget -O ./data/test_a.csv.zip https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531810/test_a.csv.zip
+wget -O ./data/test_a_sample_submit.csv https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/531810/test_a_sample_submit.csv
+unzip ./data/train_set.csv.zip -d ./data/
+unzip ./data/test_a.csv.zip -d ./data/
